cfffdb2397ff2c4cbb8bc95f20b81658df576516
akos.dombi <akos.dombi@cision.com>
2024-09-29 00:29:53 +0200

    Remove annotations

--- src/main/kotlin/com/creative_it/meetup_game_server/Game.kt
+++ src/main/kotlin/com/creative_it/meetup_game_server/Game.kt
@@ -1,10 +1,5 @@
 package com.creative_it.meetup_game_server
 
-import org.springframework.data.annotation.Id
-import org.springframework.data.keyvalue.annotation.KeySpace
+data class Game(val id: String, val users: MutableList<User> = mutableListOf())
 
-@KeySpace("games")
-data class Game(@Id val id: String, val users: MutableList<User> = mutableListOf())
-
-@KeySpace("users")
-data class User(@Id val name: String, val ready: Boolean)
+data class User(val name: String, val ready: Boolean)
