400e0a70103d5d8b93ed0cd40d7a718a1b2a5282
akos.dombi <akos.dombi@cision.com>
2024-10-08 00:17:26 +0200

    Add dummy in progress events

--- src/main/kotlin/com/creative_it/meetup_game_server/TapSnapGamingServer.kt
+++ src/main/kotlin/com/creative_it/meetup_game_server/TapSnapGamingServer.kt
@@ -6,6 +6,8 @@ import io.cloudevents.core.CloudEventUtils.mapData
 import io.cloudevents.core.v1.CloudEventBuilder
 import io.cloudevents.jackson.PojoCloudEventDataMapper
 import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlin.random.Random
+import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.seconds
 import kotlin.time.toJavaDuration
 import org.springframework.messaging.handler.annotation.DestinationVariable
@@ -47,14 +49,28 @@ class TapSnapGamingServer(
                             }
                     }
                     "com.creative_it.meetup_game_server.StartGame" -> {
-                        return@map Flux.zip(listOf(3, 2, 1).toFlux(), Flux.interval(1.seconds.toJavaDuration())) { a, _ -> a}
-                            .take(3)
+                        return@map Flux.concat(
+                            Flux.concat(
+                                Flux.zip(
+                                    listOf(3, 2, 1).toFlux(),
+                                    Flux.interval(1.seconds.toJavaDuration())
+                                ) { a, _ -> a}
+                                .take(3)
+                            )
                             .map<CloudEvent> { tick ->
                                 CloudEventBuilder(e)
                                     .withType("CountDown")
                                     .withData(objectMapper.writeValueAsBytes(tick))
                                     .build()
+                            },
+                            Flux.concat(randomInterval(1337), randomInterval(8008), randomInterval(987654))
+                            .map<CloudEvent> { tick ->
+                                CloudEventBuilder(e)
+                                    .withType("InProgress")
+                                    .withData(objectMapper.writeValueAsBytes(tick))
+                                    .build()
                             }
+                        )
                     }
                 }
                 return@map Flux.just(CloudEventBuilder(e)
@@ -69,6 +85,12 @@ class TapSnapGamingServer(
             }
             .log()
     }
+
+    private fun randomInterval(data: Int) =
+        Flux.zip(
+            Flux.just(data),
+            Flux.interval(Random.nextInt(1000, 5001).milliseconds.toJavaDuration())
+        ) { a, _ -> a }.take(1)
 
     fun <T> mapToType(e: CloudEvent): T {
         return mapData(e, PojoCloudEventDataMapper.from(
